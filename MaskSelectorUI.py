0e8f59f201ac2f8911bd7b20ba01602b4e0f71a0
atwilson0729 <atwilson0729@gmail.com>
2021-04-26 21:12:40 -0400

    Add Kivy UI

---
+++ MaskSelectorUI.py
@@ -0,0 +1,57 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+count = 0
+
+
+class MainWindow(Screen):
+    pass
+
+
+class SecondWindow(Screen):
+    pass
+
+
+class ThirdWindow(Screen):
+    pass
+
+
+class FourthWindow(Screen):
+    pass
+
+
+class FifthWindow(Screen):
+    pass
+
+
+class SixthWindow(Screen):
+    pass
+
+
+class SeventhWindow(Screen):
+    pass
+
+
+class EighthWindow(Screen):
+    pass
+
+
+class NinthWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file("my.kv")
+
+
+class MyMainApp(App):
+    def build(self):
+        return kv
+
+
+if __name__ == "__main__":
+    MyMainApp().run()
